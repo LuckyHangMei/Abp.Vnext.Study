cdf4e1953b18b499fde375f0403c027d51a18a1f
hangshilong <hangshilong@richisland.com>
2024-01-10 17:57:14 +0800

    abp学习

---
+++ AbpDemo2/src/hslAbpDemo.Application.Contracts/Dtos/TodoItemDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace hslAbpDemo;
+
+public class TodoItemDto
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; }
+}
---
+++ AbpDemo2/src/hslAbpDemo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Volo.Abp.Domain.Entities;
+
+namespace hslAbpDemo;
+
+/// <summary>
+/// BasicAggregateRoot 是创建根实体的最简单的基础类. Guid 是这里实体的主键 (Id).
+/// </summary>
+public class TodoItem:BasicAggregateRoot<Guid>
+{
+    public string Text { get; set; }
+}
